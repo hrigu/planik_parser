8ed47b8e02d1035035a7730e21367ef941fe161a
hrigu <chmuehlethaler@gmail.com>
2013-02-24 12:59:13 +0100

    Evaluator schiebt sich über die Tage und berechnet für jedes Fenster das Resultat

--- lib/planik_parser/evaluator.rb
+++ lib/planik_parser/evaluator.rb
@@ -2,16 +2,28 @@ require 'planik_parser/tree'
 module PlanikParser
 
   class Evaluator
+
     def initialize tree, situation
       @tree, @situation = tree, situation
+      @current_day_index = 0
     end
 
+    ##
+    # Berechnet für jedes Fenster das Resultat
+    # Das Fenster schiebt sich über die Situation
+    ##
     def evaluate
-      @tree.evaluate(self)
+      result = []
+      (0..(@situation.tage.size - 1 - @tree.breite)).each do |n|
+        @current_day_index = n
+        result << @tree.evaluate(self)
+      end
+      result
     end
 
-    def tag index
-      @situation.tage[index]
+    def tag relative_day_index
+      day_index = @current_day_index +relative_day_index
+      @situation.tage[day_index]
     end
   end
 end
--- spec/integration/wenn_spec.rb
+++ spec/integration/wenn_spec.rb
@@ -9,39 +9,64 @@ module PlanikParser
     context "combination of expressions" do
       context "same day" do
         context "or" do
-          let(:situation){SituationBuilder.new.build([{name: "D1", typ: "DIENST"}])}
+          let(:situation) { SituationBuilder.new.build([{name: "D1", typ: "DIENST"}]) }
 
           it "example with true" do
             tree = tree_builder.build "t0.name = D1 or t0.typ != DIENST"
             evaluator = Evaluator.new(tree, situation)
-            evaluator.evaluate.should be_true
+            evaluator.evaluate.should eq [true]
           end
 
           it "example with false" do
             tree = tree_builder.build "t0.typ = DIENST and t0.name !in (D1, D2)"
             evaluator = Evaluator.new(tree, situation)
-            evaluator.evaluate.should be_false
+            evaluator.evaluate.should eq [false]
           end
         end
       end
+
       context "two days with different dienste" do
         context "or" do
-          let(:situation){SituationBuilder.new.build([{name: "D1", typ: "DIENST"}, {name: "D2", typ: "DIENST"}])}
+          let(:situation) { SituationBuilder.new.build([{name: "D1", typ: "DIENST"}, {name: "D2", typ: "DIENST"}]) }
 
           it "example with true" do
             tree = tree_builder.build "t0.name = D1 and t1.name = D2"
             evaluator = Evaluator.new(tree, situation)
-            evaluator.evaluate.should be_true
+            evaluator.evaluate.should eq [true]
           end
           it "example with false" do
             tree = tree_builder.build "t0.typ = FERIEN or t1.name = D1"
             evaluator = Evaluator.new(tree, situation)
-            evaluator.evaluate.should be_false
+            evaluator.evaluate.should eq [false]
           end
 
         end
       end
     end
+
+    context "Mehrere Zeitfenster" do
+      let(:situation) {
+        SituationBuilder.new.build([
+                                       {name: "D1", typ: "DIENST"},
+                                       {name: "D2", typ: "DIENST"},
+                                       {name: "F", typ: "FERIEN"},
+                                       {name: "D1", typ: "DIENST"}
+                                   ]) }
+
+      it "Regel an einem Tag" do
+        tree = tree_builder.build "t0.name = D1"
+        evaluator = Evaluator.new(tree, situation)
+        evaluator.evaluate.should eq [true, false, false, true]
+      end
+      it "Regel mit zwei auf einander folgenden Tage" do
+        tree = tree_builder.build "t0.name = D1 or t1.typ = FERIEN"
+        evaluator = Evaluator.new(tree, situation)
+        evaluator.evaluate.should eq [true, true, false]
+      end
+
+    end
+
+
   end
 
 
